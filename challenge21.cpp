5f85eba3957045b08a37869e9f8a4dd4df525847
Björn Schäpers <bjoern@hazardy.de>
2025-11-13 23:42:10 +0100

    Day 21 Part 1 runs

--- challenge21.cpp
+++ challenge21.cpp
@@ -5,6 +5,7 @@
 
 #include <algorithm>
 #include <queue>
+#include <ranges>
 #include <unordered_map>
 
 namespace {
@@ -50,9 +51,14 @@ void print(Move m) noexcept {
 
 using Moves = std::vector<Move>;
 
+struct PossibleMoves {
+    std::vector<Move> OneWay;
+    std::vector<Move> OtherWay;
+};
+
 template<typename Key, std::size_t Rows, std::size_t Columns>
-auto buildDistances(std::array<std::array<Key, Columns>, Rows> map) noexcept {
-    std::unordered_map<FullMove<Key>, Moves, MyHash> distances;
+auto buildDistances(const std::array<std::array<Key, Columns>, Rows>& map) noexcept {
+    std::unordered_map<FullMove<Key>, PossibleMoves, MyHash> distances;
 
     auto addMove = [&map, &distances](std::size_t beginRow, std::size_t beginColumn, std::size_t endRow,
                                       std::size_t endColumn) noexcept {
@@ -62,48 +68,43 @@ auto buildDistances(std::array<std::array<Key, Columns>, Rows> map) noexcept {
             return;
         } //if ( move.To == Key::Invalid )
 
-        const auto numberOfHorizontalSteps = static_cast<std::int64_t>(endColumn - beginColumn);
-        const auto numberOfVerticalSteps   = static_cast<std::int64_t>(endRow - beginRow);
-        const bool isToRight               = numberOfHorizontalSteps >= 0;
-        const bool isDown                  = numberOfVerticalSteps >= 0;
+        if ( move.To == move.From ) {
+            distances.emplace(move, PossibleMoves{.OneWay{Move::Push}, .OtherWay{}});
+        } //if ( move.To == move.From )
+
+        const auto numberOfHorizontalSteps =
+            static_cast<std::int64_t>(endColumn) - static_cast<std::int64_t>(beginColumn);
+        const auto numberOfVerticalSteps = static_cast<std::int64_t>(endRow) - static_cast<std::int64_t>(beginRow);
+        const bool isToRight             = numberOfHorizontalSteps >= 0;
+        const bool isDown                = numberOfVerticalSteps >= 0;
+
         const auto horizontalSteps =
             std::views::repeat(isToRight ? Move::Right : Move::Left, std::abs(numberOfHorizontalSteps)) |
             std::ranges::to<std::vector>();
         const auto verticalSteps = std::views::repeat(isDown ? Move::Down : Move::Up, std::abs(numberOfVerticalSteps)) |
                                    std::ranges::to<std::vector>();
 
-        Moves moves;
-        Moves reversedMoves;
-        auto  forwardIter  = std::back_inserter(moves);
-        auto  reversedIter = std::back_inserter(reversedMoves);
+        //First horizontal, then vertical.
+        PossibleMoves moves{std::views::concat(horizontalSteps, verticalSteps, std::array{Move::Push}) |
+                                std::ranges::to<std::vector>(),
+                            std::views::concat(verticalSteps, horizontalSteps, std::array{Move::Push}) |
+                                std::ranges::to<std::vector>()};
 
-        if constexpr ( std::same_as<Key, NumericKey> ) {
-            if ( move.From == NumericKey::Seven && move.To == NumericKey::Three ) {
-                myPrint("");
-            } //if ( move.From == NumericKey::Seven && move.To == NumericKey::Three )
-        } //if ( std::same_as<Key, NumericKey> )
+        //From and to switched.
+        PossibleMoves reversedMoves{moves.OtherWay | std::views::transform(invert) | std::ranges::to<std::vector>(),
+                                    moves.OneWay | std::views::transform(invert) | std::ranges::to<std::vector>()};
 
-        //const bool hitsInvalid = map[beginRow][endColumn]==Key::Invalid
+        if ( map[beginRow][endColumn] == Key::Invalid ) {
+            //Would hit the empty space!
+            moves.OneWay.clear();
+            reversedMoves.OneWay.clear();
+        } //if ( map[beginRow][endColumn] == Key::Invalid )
 
-        if ( isToRight ) {
-            forwardIter = std::ranges::copy(horizontalSteps, forwardIter).out;
-        } //if ( isToRight )
-        else {
-            reversedIter = std::ranges::copy(horizontalSteps | std::views::transform(invert), reversedIter).out;
-        } //else -> if ( isToRight )
+        if ( map[endRow][beginColumn] == Key::Invalid ) {
+            moves.OtherWay.clear();
+            reversedMoves.OtherWay.clear();
+        } //if ( map[endRow][beginColumn] != Key::Invalid )
 
-        forwardIter  = std::ranges::copy(verticalSteps, forwardIter).out;
-        reversedIter = std::ranges::copy(verticalSteps | std::views::transform(invert), reversedIter).out;
-
-        if ( !isToRight ) {
-            forwardIter = std::ranges::copy(horizontalSteps, forwardIter).out;
-        } //if ( !isToRight )
-        else {
-            reversedIter = std::ranges::copy(horizontalSteps | std::views::transform(invert), reversedIter).out;
-        } //else -> if ( !isToRight )
-
-        moves.push_back(Move::Push);
-        reversedMoves.push_back(Move::Push);
         distances.emplace(move, std::move(moves));
         std::swap(move.From, move.To);
         distances.emplace(move, std::move(reversedMoves));
@@ -149,18 +150,81 @@ template<>
 const auto Distances<Move> = buildDistances(
     std::array{std::array{Move::Invalid, Move::Up, Move::Push}, std::array{Move::Left, Move::Down, Move::Right}});
 
-Moves moveRobot(Moves robotMoves) noexcept {
-    robotMoves.insert(robotMoves.begin(), Move::Push);
-    Moves ret = robotMoves | std::views::slide(2) | std::views::transform([](const auto& twoPositionRange) noexcept {
-                    const Move from = twoPositionRange.front();
-                    const Move to   = twoPositionRange.back();
-                    return Distances<Move>.find({from, to})->second;
-                }) |
-                std::views::join | std::ranges::to<std::vector>();
-    return ret;
+Moves findShortestMove(FullMove<NumericKey> move, std::size_t robots) {
+    struct X {
+        Moves MovesSoFar;
+        int   LevelsToGo;
+
+        bool operator<(const X& that) const noexcept {
+            return MovesSoFar.size() > that.MovesSoFar.size();
+        }
+
+        bool operator==(const X& that) const noexcept = default;
+    };
+
+    struct Hash {
+        static std::size_t operator()(const X& entry) noexcept {
+            std::hash<int> h;
+            return h(static_cast<int>(entry.MovesSoFar.size())) ^ h(entry.LevelsToGo);
+        }
+    };
+
+    static std::unordered_map<X, Moves, Hash> cache;
+    std::priority_queue<X>                    queue;
+
+    const auto& possibleMoves = Distances<NumericKey>.at(move);
+    if ( !possibleMoves.OneWay.empty() ) {
+        queue.emplace(possibleMoves.OneWay, robots);
+    } //if ( !possibleMoves.OneWay.empty() )
+
+    if ( !possibleMoves.OtherWay.empty() ) {
+        queue.emplace(possibleMoves.OtherWay, robots);
+    } //if ( !possibleMoves.OtherWay.empty() )
+
+    throwIfInvalid(!queue.empty());
+
+    while ( !queue.empty() ) {
+        const auto current = queue.top();
+        queue.pop();
+
+        if ( current.LevelsToGo == 0 ) {
+            return std::move(current.MovesSoFar);
+        } //if ( current.LevelsToGo == 0 )
+
+        if ( const auto iter = cache.find(current); iter != cache.end() ) {
+            queue.emplace(iter->second, current.LevelsToGo - 1);
+            continue;
+        } //if ( const auto iter = cache.find(current); iter != cache.end() )
+
+        auto addToState = [end = std::size(current.MovesSoFar), nextLevel = current.LevelsToGo - 1, &queue, &current](
+                              this const auto& self, std::size_t index = 0, const Moves& moves = {}) noexcept -> void {
+            if ( index == end ) {
+                queue.emplace(moves, nextLevel);
+                cache.emplace(current, moves);
+                return;
+            } //if ( index == end )
+
+            FullMove<Move> localMove{index == 0 ? Move::Push : current.MovesSoFar[index - 1],
+                                     current.MovesSoFar[index]};
+            const auto&    nextPossibleMoves = Distances<Move>.at(localMove);
+
+            if ( !nextPossibleMoves.OneWay.empty() ) {
+                self(index + 1, std::views::concat(moves, nextPossibleMoves.OneWay) | std::ranges::to<std::vector>());
+            } //if ( !nextPossibleMoves.OneWay.empty() )
+
+            if ( !nextPossibleMoves.OtherWay.empty() ) {
+                self(index + 1, std::views::concat(moves, nextPossibleMoves.OtherWay) | std::ranges::to<std::vector>());
+            } //if ( !nextPossibleMoves.OtherWay.empty() )
+            return;
+        };
+
+        addToState();
+    } //while ( !queue.empty() )
+
+    fail();
 }
 
-std::int64_t getShortestSequenceLength(std::string_view code) {
+std::int64_t getShortestSequenceLength(std::string_view code, std::size_t robots) {
     throwIfInvalid(code.size() == 4);
     throwIfInvalid(code.back() == 'A');
     std::array<NumericKey, 4> sequence;
@@ -168,32 +232,26 @@ std::int64_t getShortestSequenceLength(std::string_view code) {
     std::ranges::transform(code.substr(0, 3), sequence.begin(),
                            [](char c) noexcept { return static_cast<NumericKey>(c - '0'); });
 
-    Moves moves = sequence | std::views::transform([at = NumericKey::A](NumericKey to) mutable noexcept {
-                      return Distances<NumericKey>.find({std::exchange(at, to), to})->second;
-                  }) |
-                  std::views::join | std::ranges::to<std::vector>();
+    Moves      moves;
+    NumericKey at = NumericKey::A;
 
-    myPrint("Moves: ");
-    std::ranges::for_each(moves, &print);
+    //myPrint("Moves:");
+    for ( auto to : sequence ) {
+        auto nextMove = findShortestMove({at, to}, robots);
+        //myPrint(" ");
+        std::ranges::for_each(nextMove, &print);
+        myFlush();
+        moves.append_range(std::move(nextMove));
+        at = to;
+    } //for ( auto to : sequence )
     myPrint("\n");
 
-    moves = moveRobot(std::move(moves));
-
-    myPrint("Moves: ");
-    std::ranges::for_each(moves, &print);
-    myPrint("\n");
-
-    moves = moveRobot(std::move(moves));
-
-    myPrint("Moves: ");
-    std::ranges::for_each(moves, &print);
-    myPrint("\n");
-
-    return static_cast<std::int64_t>(moves.size());
+    return std::ssize(moves);
 }
 
+template<std::size_t Robots>
 std::int64_t getComplexity(std::string_view code) noexcept {
-    auto length  = getShortestSequenceLength(code);
+    auto length  = getShortestSequenceLength(code, Robots);
     auto numeric = convert(code);
     myPrint("Length: {:3d} Numeric: {:3d}\n", length, numeric);
     return length * numeric;
@@ -201,11 +259,12 @@ std::int64_t getComplexity(std::string_view code) noexcept {
 } //namespace
 
 bool challenge21(const std::vector<std::string_view>& input) {
-    const auto sum1 = std::ranges::fold_left(input | std::views::transform(getComplexity), 0, std::plus<>{});
+    const auto sum1 = std::ranges::fold_left(input | std::views::transform(getComplexity<2>), 0, std::plus<>{});
     myPrint(" == Result of Part 1: {:d} ==\n", sum1);
+    myFlush();
 
-    const auto sum2 = 0;
+    const auto sum2 = std::ranges::fold_left(input | std::views::transform(getComplexity<25>), 0, std::plus<>{});;
     myPrint(" == Result of Part 2: {:d} ==\n", sum2);
 
-    return sum1 == 3858 && sum2 == 636'350'496'972'143;
+    return sum1 == 238078 && sum2 == 636'350'496'972'143;
 }
